7112a93bcfcad8be319425c97b6a9c79f0d82e08
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-12-22 05:14:33 +0600

    java practice add daily task. string and hello world push

---
+++ JavaVariableString.java
@@ -0,0 +1,13 @@
+package com.example.learnjava;
+
+public class JavaVariableString {
+    public static void main(String[] args) {
+//        System.out.println("hi bro!");
+//        System.out.println("hi sis!");
+//        System.out.println("hi kiddddd!");
+        //variable == bucket in memory
+        String name = "Ratul";
+
+        System.out.println("Hey dude its me "+name+".");
+    }
+}
---
+++ MyClass.java
@@ -0,0 +1,11 @@
+package com.example.learnjava;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Another day bro! Hello again :))");
+        System.out.println("Hey today is a beautiful day :)))");
+        System.out.println("Our instructor is quiet cool!");
+        System.out.println("there is a lot of things to do today whatever it happens :))");
+    }
+}
