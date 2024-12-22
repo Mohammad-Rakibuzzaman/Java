39811b6729818ee8314bd8ef8cc13f91d0293131
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-12-22 19:08:25 +0600

    add int chapter

---
+++ JavaVariableInteger.java
@@ -0,0 +1,14 @@
+package com.example.learnjava;
+
+public class JavaVariableInteger {
+    public static void main(String[] args) {
+
+        String name = "Ratul";
+        int age = 27;
+        int year = 1997;
+
+        System.out.println("I'm "+name+" and I'm "+age+" years old.");
+        System.out.println(year);
+
+    }
+}
